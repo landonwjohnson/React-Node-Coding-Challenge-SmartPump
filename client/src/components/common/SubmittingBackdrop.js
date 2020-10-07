140d9ec5d011424c9223671761fa00ede429a728
“Landon <landonwjohnson@gmail.com>
2020-10-07 11:52:50 -0600

    saved submitting backdrop

--- client/src/components/common/SubmittingBackdrop.js
+++ client/src/components/common/SubmittingBackdrop.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import styled from 'styled-components'
-import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import React from "react";
+import styled from "styled-components";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const StyledBackdrop = styled(Backdrop)`
-    z-index: 10000 !important;
-    color: white;
-`
+  z-index: 10000 !important;
+  color: white;
+`;
 
-
- function SubmittingBackdrop() {
-    return (
-        <StyledBackdrop open={true}>
-  <CircularProgress color="inherit" />
-</StyledBackdrop>
-    )
+function SubmittingBackdrop() {
+  return (
+    <StyledBackdrop open={true}>
+      <CircularProgress color="inherit" />
+    </StyledBackdrop>
+  );
 }
 
-export default SubmittingBackdrop
+export default SubmittingBackdrop;
