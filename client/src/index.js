e0e64ca98f887fb5bd2a9aa5bcb52a365921641e
“Landon <landonwjohnson@gmail.com>
2020-10-07 21:13:11 -0600

    removed app.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import App from "./App";
 import Login from "./components/Login";
 import UserAccount from "./components/UserAccount";
-import { AuthProvider } from "./context/AuthContext";
-import { ErrorsProvider } from "./context/ErrorsContext";
 import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyle } from "./components/Layout/GlobalStyle";
 import AuthLayout from "./components/Layout/AuthLayout";
@@ -14,14 +11,19 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      
-          <Router>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/login" render={(props) =>  <Login {...props} />} />
-
-            <Route exact path="/account" render={(props) => <AuthLayout title={"Account"} {...props}> <UserAccount {...props} /> </AuthLayout>}  />
-          </Router>
-     
+      <Router>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/login" render={(props) => <Login {...props} />} />
+        <Route
+          exact
+          path="/account"
+          render={(props) => (
+            <AuthLayout title={"Account"} {...props}>
+              <UserAccount {...props} />
+            </AuthLayout>
+          )}
+        />
+      </Router>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
